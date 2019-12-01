5c4ef29a4ca1c6bf1133b8bd05b7357504dd2c54
Denis <den.belotserkovets@gmail.com>
2019-12-01 15:03:25 +0200

    feat(view): log out

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,10 +6,24 @@ import { AuthGuardService } from './services/auth-guard.service';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @NgModule({
     declarations: [],
-    exports: [CommonModule, HttpClientModule, ReactiveFormsModule, FormsModule, NzFormModule, NzInputModule, NzButtonModule],
+    exports: [
+        CommonModule,
+        HttpClientModule,
+        ReactiveFormsModule,
+        FormsModule,
+        NzFormModule,
+        NzInputModule,
+        NzButtonModule,
+        NzLayoutModule,
+        NzIconModule,
+        NzMenuModule,
+    ],
     providers: [AuthGuardService],
 })
 export class SharedModule {}
