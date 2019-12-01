3ec22fb330df8cc39ce14471b732dc02f7a99707
Denis <den.belotserkovets@gmail.com>
2019-12-01 13:56:35 +0200

    feat(view): login page

--- src/app/content/login/login.component.ts
+++ src/app/content/login/login.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+    constructor(private fb: FormBuilder, private router: Router) {}
+
+    public form!: FormGroup;
+
+    public ngOnInit(): void {
+        this.form = this.fb.group({
+            userName: [null, [Validators.required]],
+            password: [null, [Validators.required]],
+        });
+    }
+
+    public submitForm(): void {
+        this.form.markAsDirty();
+        this.form.updateValueAndValidity();
+        this.setUser(this.form.value.userName);
+        this.router.navigate(['home']);
+    }
+
+    private setUser(userName: string): void {
+        localStorage.setItem('user', userName);
+    }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { AuthGuardService } from './services/auth-guard.service';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @NgModule({
     declarations: [],
-    exports: [CommonModule, HttpClientModule, ReactiveFormsModule, FormsModule],
+    exports: [CommonModule, HttpClientModule, ReactiveFormsModule, FormsModule, NzFormModule, NzInputModule, NzButtonModule],
     providers: [AuthGuardService],
 })
 export class SharedModule {}
